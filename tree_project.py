da0a66a654b9168afc331351a7c0d1e7164c95a3
Virtu <maksimsarsov777@gmail.com>
2024-06-01 12:00:00 +0300

    Начало создания проекта. Файл tree_project.py

---
+++ tree_project.py
@@ -0,0 +1,10 @@
+import os
+
+def print_tree(start_path, exclude_dirs=None, prefix=""):
+    
+    pass
+
+# Укажите путь к проекту
+project_path = "C:\\Programs\\audio_test"
+print(f"Дерево проекта для: {project_path}")
+print_tree(project_path)
