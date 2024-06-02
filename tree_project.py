9876e849a7dd23877a15aedf46dbfa151a797358
Virtu <maksimsarsov777@gmail.com>
2024-06-02 09:34:42 +0300

    Добавлена обработка файлов

--- tree_project.py
+++ tree_project.py
@@ -1,8 +1,17 @@
 import os
 
 def print_tree(start_path, exclude_dirs=None, prefix=""):
-    
-    pass
+    if exclude_dirs is None:
+        exclude_dirs = {'.git', 'venv', 'pycache'}
+
+    # Получаем список всех элементов, исключая указанные папки
+    entries = sorted(os.listdir(start_path))
+    entries = [entry for entry in entries if entry not in exclude_dirs]
+
+    # Разделяем на папки и файлы
+    dirs = [entry for entry in entries if os.path.isdir(os.path.join(start_path, entry))]
+    files = [entry for entry in entries if not os.path.isdir(os.path.join(start_path, entry))]
+
 
 # Укажите путь к проекту
 project_path = "C:\\Programs\\audio_test"
