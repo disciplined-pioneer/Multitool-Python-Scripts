0a26e2c67d5028ce81c8c99aca2166acace7b460
Virtu <maksimsarsov777@gmail.com>
2024-06-02 12:42:32 +0300

    Полностью закончен проект дерева проекта

--- tree_project.py
+++ tree_project.py
@@ -12,6 +12,18 @@ def print_tree(start_path, exclude_dirs=None, prefix=""):
     dirs = [entry for entry in entries if os.path.isdir(os.path.join(start_path, entry))]
     files = [entry for entry in entries if not os.path.isdir(os.path.join(start_path, entry))]
 
+    # Объединяем папки и файлы
+    sorted_entries = dirs + files
+
+    for i, entry in enumerate(sorted_entries):
+        entry_path = os.path.join(start_path, entry)
+        connector = "└───" if i == len(sorted_entries) - 1 else "├───"
+
+        print(f"{prefix}{connector} {entry}")
+
+        if os.path.isdir(entry_path):
+            new_prefix = f"{prefix}    " if i == len(sorted_entries) - 1 else f"{prefix}│   "
+            print_tree(entry_path, exclude_dirs, new_prefix)
 
 # Укажите путь к проекту
 project_path = "C:\\Programs\\audio_test"
